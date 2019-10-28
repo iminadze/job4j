6e65cae6e112ec923d6047a0ec8c702ea0a8f870
Ilya Inadze <iminadze90@gmail.com>
2019-10-28 15:17:37 +0300

    5.7. Ипотека. [#156315]

--- chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -9,7 +9,7 @@ public class MortgageTest {
     @Test
     public void when1Year() {
         Mortgage mortgage = new Mortgage();
-        int year = mortgage.year(1000, 1200, 1);
+        int year = mortgage.year(1000, 1200, 2);
         assertThat(year, is(1));
     }
 
