c312eed900a5e6d2393356b5ba99f7c367780257
Ilya Inadze <iminadze90@gmail.com>
2019-10-30 11:38:41 +0300

    5.8. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,58 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс для вычисления арифметических операций + - * /.
+ * @author iminadze
+ * @since 30.10.19
+ * @version 1
+ */
+
 public class Calculator {
+    /**
+     *  Сложение.
+     *@param first первый аргумент.
+     * @param second второй аргумент.
+     * @return результат.
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     *  Вычитание.
+     *@param first первый аргумент.
+     * @param second второй аргумент.
+     * @return результат.
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
+    /**
+     * Умножение.
+     *@param first первый аргумент.
+     * @param second второй аргумент.
+     * @return результат.
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
+    /**
+     *  Деление.
+     *@param first первый аргумент.
+     * @param second второй аргумент.
+     * @return результат.
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
+
+    /**
+     * Main
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         subtract(6, 3);
