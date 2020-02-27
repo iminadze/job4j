5443923d6a826a6883839f218d09623561085df8
Ilya Inadze <iminadze90@gmail.com>
2020-02-27 15:05:18 +0300

    6.7.2. Моно столбец и моно строка в матрице

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,53 +1,24 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
-    public static boolean isWin(char[][] board) {
-        boolean result = false;
-        for (int row = 0; row < board.length; row++) {
-            int winCount = 0;
-                char sign = board[row][row];
-                System.out.print(sign);
-                if (board[row][row] == 'X') {
-                    winCount++;
-                }
-                if (winCount == board.length) {
-                    result = true;
-                    break;
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[row][cell] != 'X') {
+                result = false;
+                break;
             }
-            System.out.println();
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        char[][] hasWinVertical = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean win = isWin(hasWinVertical);
-        System.out.println("A board has a winner : " + win);
-        System.out.println();
-        char[][] hasWinHor = {
-                {'_', '_', '_', '_', '_'},
-                {'X', 'X', 'X', 'X', 'X'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-        };
-        boolean winHor = isWin(hasWinHor);
-        System.out.println("A board has a winner : " + winHor);
-        System.out.println();
-        char[][] notWin = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', 'X', '_', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean lose = isWin(notWin);
-        System.out.println("A board has a winner : " + lose);
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
