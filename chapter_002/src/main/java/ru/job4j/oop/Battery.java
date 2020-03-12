20ff6d4b85fdd665911eddbd40972f233bc170b0
Ilya Inadze <iminadze90@gmail.com>
2020-03-12 19:13:16 +0300

    1.7. Состояние объекта.[#188544]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public Battery(int size) {
+       this.load = size;
+    }
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+    public static void main(String[] args) {
+        Battery first = new Battery(60);
+        Battery second = new Battery(40);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        second.exchange(first);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+
+    }
+}
