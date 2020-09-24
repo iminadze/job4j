870c670b052855cb9a2542930598ff03acdec216
Ilya Inadze <iminadze90@gmail.com>
2020-09-24 19:52:55 +0300

    4. Сравнение моделей. Метод equals. [#188560]

--- chapter_002/src/main/java/ru/job4j/pojo/License.java
+++ chapter_002/src/main/java/ru/job4j/pojo/License.java
@@ -14,7 +14,10 @@ public class License {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         License license = (License) o;
-        return code.equals(license.code);
+        return Objects.equals(owner, license.owner) &&
+                Objects.equals(model, license.model) &&
+                Objects.equals(code, license.code) &&
+                Objects.equals(created, license.created);
     }
 
     @Override
